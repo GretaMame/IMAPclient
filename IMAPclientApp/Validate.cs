af43280e0ee65bb210e3b04f97a86ceb3bed0d84
Greta <gretamameniskyte@gmail.com>
2017-04-03 16:31:37 +0300

    Add files via upload

---
+++ IMAPclientApp/Validate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+
+namespace IMAPclientApp
+{
+    class Validate
+    {
+        public Validate()
+        {
+
+        }
+
+        public static bool ValidateEmail(string email)
+        {
+            try
+            {
+                var emailAddr = new MailAddress(email);
+                return email == emailAddr.Address;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
